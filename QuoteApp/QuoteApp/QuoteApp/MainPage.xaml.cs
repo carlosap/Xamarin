67b359803d896d330bdc249b0e6e5ffac421ef6c
Carlos Perez <carlos.perez@NOVASP.local>
2017-07-03 23:33:05 -0400

    init quoteapp

---
+++ QuoteApp/QuoteApp/QuoteApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuoteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _index = 0;
+        private string[] _quotes = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            currentQuote.Text = _quotes[_index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
+
+            currentQuote.Text = _quotes[_index];
+        }
+    }
+}
